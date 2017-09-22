0bdb516e8f917843f9e33457c624d7f91dc8530e
rajrin@yahoo.com <raj@acloudfan.com>
2017-09-22 08:40:16 -0400

    updated

--- contracts/CourseTokenV3.sol
+++ contracts/CourseTokenV3.sol
@@ -2,10 +2,12 @@ pragma solidity ^0.4.4;
 
 import './CourseTokenV2.sol';
 // V3 - inherits from V2
-// Implement the remaining functions of ERC20 specs to make this version compatible 
-// with Tools supporting ERC20
-// This is JUST A SHELL - Students should try to implement exercise on their own
-
+// Add the admin functions * to be done
+//
+// This is JUST A SHELL - Students should try to implement it as an exercise on their own
+//
+// Refer to example admin functions below
+//
 // Implement the administrative functions
 contract CourseTokenV3 is CourseTokenV2 {
 
@@ -13,4 +15,19 @@ contract CourseTokenV3 is CourseTokenV2 {
     // constructor
     // Feel free to change the constructor
   }
+
+  function increaseSupply(uint additional) returns (bool success) {
+    // 1. Only owner can take this action
+    // 2. Caller gets the ownership of additional coins
+    return true;
+  }
+
+  function decreaseSupply(uint additional) returns (bool success) {
+    // 1. Only owner can take this action
+    // 2. Caller's token are taken for removal
+    // 3. What happens when caller does not have sufficient balance?
+    return true;
+  }
+
+  // Think of other admin functions you would like to implement
 }
